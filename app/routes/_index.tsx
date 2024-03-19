3587d913700e5a94c08c099eeee353caf72c964d
McDuck <rami.lejmi@helsinki.fi>
2024-03-19 22:37:38 +0200

    x

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,52 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Laskuja" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+
+import { useEffect, useState } from "react";
+import { Link } from "@remix-run/react";
+
+const tasks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
 export default function Index() {
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [clientSide, setClientSide] = useState(false);
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("completedTasks") || "[]");
+    setCompletedTasks(storedTasks);
+    setClientSide(true); // Indicate that we're now on the client-side
+  }, []);
+
+  
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+    <div className="flex justify-center">
+      LALALA
+      <div className="grid grid-cols-2 gap-4 p-4">
+        {tasks.map((task, index) => {
+          let isTaskCompleted = false
+          if (clientSide) {
+            // Now we check localStorage only if we're sure we're on the client-side
+            isTaskCompleted = localStorage.getItem(`laskut-task-${task}`) === 'True';
+          }
+          console.log(typeof isTaskCompleted)
+          return (
+            <Link
+              to={`/${task}`}
+              key={index}
+              className={`w-24 h-24 flex items-center justify-center text-center border rounded-lg cursor-pointer ${isTaskCompleted ? "bg-green-200" : completedTasks.includes(task) ? "bg-gray-200" : "bg-white shadow-xl"}`}
+            >
+              {task}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
